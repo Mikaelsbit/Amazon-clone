5d78165e124f3f73f8da8202f979d3a9787eac7f
Michael <micahelbiru@gmail.com>
2025-03-09 14:07:14 -0400

    cart page 3

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -9,7 +9,9 @@ import { DataContext } from "../DataProvider/DataProvider";
 const Header = () => {
 
   const [{basket} , dispatch] = useContext (DataContext)
-  console.log(basket.length)
+  const totalItem = basket?.reduce((amount,item) => {
+    return item.amount + amount
+  }, 0)
   return (
     <section className={classes.fixed}>
       <section>
@@ -65,7 +67,7 @@ const Header = () => {
             <Link to="/cart" className={classes.cart}>
               {/* icon */}
               <MdOutlineShoppingCart size ={35} />
-              <span>{basket.length}</span>
+              <span>{totalItem}</span>
             </Link>
           </div>
         </div>
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -4,12 +4,30 @@ import { DataContext } from "../../Components/DataProvider/DataProvider";
 import ProductCard from "../../Components/Products/ProductCard";
 import CurrencyFormat from "../../Components/CurrencyFormat/CurrencyFormats";
 import { Link } from "react-router-dom";
-import classes from './cart.module.css';
+import classes from "./cart.module.css";
+import {Type} from '../../utility/action.type'
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
 function Cart() {
   const [{ basket, user }, dispatch] = useContext(DataContext);
-  const total = basket.reduce((amount , item) => {
-    return item.price + amount
-  }, 0)
+  const total = basket.reduce((amount, item) => {
+    return item.price * item.amount + amount;
+  }, 0);
+
+  const increment = (item) => {
+    dispatch({
+      type: Type.ADD_TO_BASKET,
+      item
+    })
+  }
+
+  const decrement = (id) => {
+    dispatch({
+      type: Type.REMOVE_FROM_BASKET,
+      id
+
+    })
+  }
   return (
     <LayOut>
       <section className={classes.container}>
@@ -21,32 +39,44 @@ function Cart() {
           {basket?.length == 0 ? (
             <p>Oopps ! No item in your cart</p>
           ) : (
-            basket?.map((item,i) => {
-              return <ProductCard 
-              product={item}
-              renderDesc={true}
-              flex={true}
-              key={i}
-              renderAdd={false}
-              />;
+            basket?.map((item, i) => {
+              return (
+                <section className={classes.cart_product}>
+                  <ProductCard
+                    product={item}
+                    renderDesc={true}
+                    flex={true}
+                    key={i}
+                    renderAdd={false}
+                  />
+                  <div className={classes.btn_container}>
+                    <button onClick={() => increment(item)}>
+                    <IoIosArrowUp size={20}/>
+                    </button>
+
+                    <span>{item.amount}</span>
+                    <button onClick={() => decrement(item.id)}>
+                    <IoIosArrowDown size={20} />
+                    </button>
+                  </div>
+                </section>
+              );
             })
           )}
         </div>
-        {
-          basket?.length !==0 && (
-            <div className={classes.subtotal}>
-              <div>
-                <p>subtotal ({basket?.length} items)</p>
-                <CurrencyFormat amount={total} />
-              </div>
-              <span>
-                <input type="checkbox" />
-                <small>This order contains gift</small>
-              </span>
-              <Link to="/payments">Continue to checkout</Link>
+        {basket?.length !== 0 && (
+          <div className={classes.subtotal}>
+            <div>
+              <p>subtotal ({basket?.length} items)</p>
+              <CurrencyFormat amount={total} />
             </div>
-          )
-        }
+            <span>
+              <input type="checkbox" />
+              <small>This order contains gift</small>
+            </span>
+            <Link to="/payments">Continue to checkout</Link>
+          </div>
+        )}
       </section>
     </LayOut>
   );
