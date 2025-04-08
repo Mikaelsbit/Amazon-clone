35a6b3f0c8134a8977451a4c3e85935b8137ba6b
Michael <micahelbiru@gmail.com>
2025-04-07 21:15:39 -0400

    Final touches

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-c612c/us-central1/api"
+  baseURL: "https://amazon-api-deploy-uslq.onrender.com"
 })
 
 export {axiosInstance}
